1d811e3b1c51664f1c0b62c4905e8fafbd7b7da9
danielsehr <daniel.serr7028@gmail.com>
2026-04-15 21:56:14 +0200

    added setup() method, added dataloader methods, added argument, renamed metadata variable for concatening

--- dataset/datamodule_meta_concat.py
+++ dataset/datamodule_meta_concat.py
@@ -11,7 +11,7 @@ from torchmetrics.classification import MulticlassJaccardIndex
 
 from sklearn.model_selection import train_test_split
 
-from dataset.dataset import SegmentationDataset
+from dataset.dataset_meta_concat import SegmentationDatasetMetaConcat 
 from models.deeplabv3 import build_deeplabv3plus
 from utils.albumentation import augment, transform
 from utils.losses import FocalDiceLoss
@@ -22,17 +22,28 @@ from typing import Any
 class DeepLabV3PlusMetaConcat(pl.LightningModule):
     def __init__(
         self,
+        data_dir: str | Path,
         num_classes: int,
         in_channels: int,
         meta_dim: int,
+        augment: Any,
+        transform: Any,
+        batch_size: int,
+        num_workers: int,
         ignore_index: int = 0,
         ):
         super().__init__()
-        
+
+        self.data_dir = Path(data_dir)
         self.meta_dim = meta_dim
         self.num_classes = num_classes
         self.in_channels = in_channels
+        self.augment = augment
+        self.transform = transform
         self.ignore_index = ignore_index
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.save_hyperparameters()        
         
         
         # Model
@@ -74,6 +85,70 @@ class DeepLabV3PlusMetaConcat(pl.LightningModule):
         )
         
         
+    def setup(self, stage=None):
+        if stage in ("fit", None):
+            self.full_dataset = SegmentationDatasetMetaConcat(
+                images_dir = self.data_dir / "flair_1_toy_aerial_train",
+                masks_dir = self.data_dir / "flair_1_toy_labels_train_remap",
+                meta_json_dir = self.data_dir / "flair_1_metadata_aerial", 
+                augment = self.augment,
+                transform = self.transform,
+            )
+            print(f"Lenght full dataset: {len(self.full_dataset)}")
+            
+            # Split dataset
+            self.train_dataset, self.val_dataset = random_split(
+                dataset = self.full_dataset,
+                lengths = [0.8, 0.2],
+                generator = torch.Generator().manual_seed(42)
+            )
+            print(f"Lenght training dataset: {len(self.train_dataset)}")
+            print(f"Lenght validation dataset: {len(self.val_dataset)}")
+            
+            # Set augment for val datasets to None
+            self.val_dataset.augment = None
+            
+            
+        if stage in ("test", None):
+            self.test_dataset = SegmentationDatasetMetaConcat(
+                images_dir = self.data_dir / "flair_1_toy_aerial_test",
+                masks_dir = self.data_dir / "flair_1_toy_labels_test_remap",
+                meta_json_dir = self.data_dir / "flair_1_metadata_aerial", 
+                augment = None,
+                transform = self.transform,
+            )
+            print(f"Lenght test dataset: {len(self.test_dataset)}")
+            
+            
+            # Define dataloaders
+    def train_dataloader(self):
+        return DataLoader(
+            dataset=self.train_dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=self.num_workers,
+            pin_memory=True,
+            # persistent_workers=True,
+            prefetch_factor=2
+        )
+    
+    def val_dataloader(self):
+        return DataLoader(
+            dataset=self.val_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=True
+        )
+    
+    def test_dataloader(self):
+        return DataLoader(
+            dataset=self.test_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=True
+        )
+        
+        
     def forward(self, x, meta):
         # x: (B, C, H, W)
         # meta: (B, meta_dim)
@@ -101,10 +176,10 @@ class DeepLabV3PlusMetaConcat(pl.LightningModule):
         
     def training_step(self, batch, batch_idx):
         images = batch["image"]
-        meta = batch["meta"]
+        bin_gh = batch["binary_geohash"]
         masks = batch["mask"]
 
-        logits = self(images, meta)
+        logits = self(images, bin_gh)
         loss = self.criterion(logits, masks)
 
         preds = torch.argmax(logits, dim=1)
@@ -117,10 +192,10 @@ class DeepLabV3PlusMetaConcat(pl.LightningModule):
     
     def validation_step(self, batch, batch_idx):
         images = batch["image"]
-        meta = batch["meta"]
+        bin_gh = batch["binary_geohash"]
         masks = batch["mask"]
         
-        logits = self(images, meta)
+        logits = self(images, bin_gh)
         loss = self.criterion(logits, masks)
         
         preds = torch.argmax(logits, dim=1)
@@ -133,10 +208,10 @@ class DeepLabV3PlusMetaConcat(pl.LightningModule):
     
     def test_step(self, batch, batch_idx):
         images = batch["image"]
-        meta = batch["meta"]
+        bin_gh = batch["binary_geohash"]
         masks = batch["mask"]
         
-        logits = self(images, meta)
+        logits = self(images, bin_gh)
         loss = self.criterion(logits, masks)
         
         preds = torch.argmax(logits, dim=1)
@@ -180,8 +255,8 @@ class DeepLabV3PlusMetaConcat(pl.LightningModule):
 
     def predict_step(self, batch, batch_idx):
         images = batch["image"]
-        meta = batch["meta"]
+        bin_gh = batch["binary_geohash"]
         
-        logits = self(images, meta)
+        logits = self(images, bin_gh)
         
         return torch.argmax(logits, dim=1)
